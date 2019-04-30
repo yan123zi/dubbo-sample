97913188b80f88329809db777742d6c081c84c6a
yan123zi <673343330@qq.com>
2019-04-30 16:15:03 +0800

    添加子模块

---
+++ dubbo-commons-mapper/src/main/java/mapper/MyMapper.java
@@ -0,0 +1,11 @@
+package mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author YZJ
+ * @date 2019/4/30 - 15:58
+ */
+public interface MyMapper extends BaseMapper {
+}
